c1426a595d46bca289a70f3946b997f817c74c82
Eric Mendes Dantas <ericdantas0@gmail.com>
2015-10-18 21:25:47 -0200

    .;.;.

--- client/os_cmp.ts
+++ client/os_cmp.ts
@@ -1,11 +1,13 @@
 /// <reference path="../typings/tsd.d.ts" />
 
 import {Component} from 'angular2/angular2';
+import {LoginCmp} from 'client/login/login_cmp.js';
 
 @Component({
   selector: 'os-cmp',
   templateUrl: 'client/os.html',
-  styleUrls: ['client/os.css']
+  styleUrls: ['client/os.css'],
+  directives: [LoginCmp]
 })
 export class OsCmp {
   name: string = 'os-cmp'
